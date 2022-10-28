ef079bbdd69d6e6e88592809f5c27d3ba7512405
Patrick Kim <patrick.yrkim@gmail.com>
2022-10-28 00:40:21 -0700

    Update Feed component with sidebar and videos

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+
+import { Sidebar, Videos } from './'
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack
+      sx={{
+        flexDirection: { sx: 'column', md: 'row' }
+      }}
+    >
+      <Box
+        sx={{
+          height: { sx: 'auto', md: '92vh' },
+          borderRight: '1px solid #3d3d3d',
+          px: { sx: 0, md: 2 }
+        }}
+      >
+        <Sidebar />
+      </Box>
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography variant='h4' fontWeight='bold' mb={2} sx={{ cpolor: 'white' }}>
+          New <span style={{ color: '#FC1503' }}>videos</span>
+        </Typography>
+        <Videos 
+          videos={[]}
+        />
+      </Box>
+    </Stack>
   )
 }
 
