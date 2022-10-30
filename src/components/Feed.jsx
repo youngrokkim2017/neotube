988453a6725d5f238b1f69bfe287ab16b0be68ad
Patrick Kim <patrick.yrkim@gmail.com>
2022-10-29 23:49:10 -0700

    Add api call for video data

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,8 +2,17 @@ import React, { useState, useEffect } from 'react'
 import { Box, Stack, Typography } from '@mui/material'
 
 import { Sidebar, Videos } from './'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState('New')
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items))
+  }, [selectedCategory])
+
   return (
     <Stack
       sx={{
@@ -17,14 +26,17 @@ const Feed = () => {
           px: { sx: 0, md: 2 }
         }}
       >
-        <Sidebar />
+        <Sidebar 
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
       </Box>
       <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
         <Typography variant='h4' fontWeight='bold' mb={2} sx={{ cpolor: 'white' }}>
-          New <span style={{ color: '#FC1503' }}>videos</span>
+          {selectedCategory} <span style={{ color: '#FC1503' }}>videos</span>
         </Typography>
         <Videos 
-          videos={[]}
+          videos={videos}
         />
       </Box>
     </Stack>
