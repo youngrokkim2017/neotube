bd6d7b90957789f5ecb2cb3bba2e28f521098216
Patrick Kim <patrick.yrkim@gmail.com>
2022-11-01 23:40:48 -0700

    Update channelitem component

--- src/components/ChannelItem.jsx
+++ src/components/ChannelItem.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+
+import { Videos, ChannelCard } from './'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
 
 const ChannelItem = () => {
+  const { id } = useParams();
+  const [channelDetail, setChannelDetail] = useState(null)
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`)
+      .then((data) => setChannelDetail(data?.items[0]))
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
+      .then((data) => setVideos(data?.items))
+  }, [id])
+  
   return (
-    <div>ChannelItem</div>
+    <Box minHeight='95vh'>
+      <Box>
+        <div
+          style={{
+            background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
+            zIndex: 10,
+            height: '300px'
+          }}
+        />
+        <ChannelCard channelDetail={channelDetail} />
+      </Box>
+    </Box>
   )
 }
 
